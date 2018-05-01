55986c45c712abf6a2b2ea2d61b355d939c90832
Mohnish G J <mail4mohnish@gmail.com>
2018-05-01 16:21:27 +0530

    Calculate the sum of elements of a list using recursion and pattern matching

--- lists/list.ex
+++ lists/list.ex
@@ -20,6 +20,22 @@ defmodule Lists do
   def len([]), do: 0
   def len([_h | t]), do: 1 + len(t)
 
+
+  @doc """
+  Calculate the sum of a list by recursion
+
+  ## Examples
+
+      iex> Lists.sum([])
+      0
+
+      iex> Lists.sum([4, 3, 2, 5])
+      14
+  """
+
+  def sum([]), do: 0
+  def sum([h | t]), do: h + sum(t)
+
   @doc """
   Add the ability to calculate the double of each element in a list
 
